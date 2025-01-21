31b79260d8928f31402c54400a35acfb059c1a6a
Kaarel Moppel <kaarel.moppel@gmail.com>
2025-01-21 13:23:47 +0200

    Format some analyze SQLs
    And remove output numbers

--- analyze_results.sql
+++ analyze_results.sql
@@ -5,46 +5,69 @@ with q as (
 	  (select avg(mean_exec_time)::numeric(9, 2) from pgss_results where parallel_leader_participation = 'off') as plp_off
 )
 select 100.0 * ((q.plp_off - q.plp_on) / q.plp_on) as diff from q;
--- -3.538868875728028982000
 
 
+-- summary per scale / partitions
+SELECT
+    scale,
+    partitions,
+    avg(mean_exec_time / 1000.0)::int mean_exec_time_ms,
+    avg(pct)::numeric(7, 1) AS no_leader_participation_speedup_pct
+FROM (
+    SELECT
+        *,
+        lag AS mean_exec_time_lag,
+        (100.0 * ((mean_exec_time - lag) / lag))::numeric(7, 1) pct
+    FROM (
+        SELECT
+            *,
+            lag(mean_exec_time) OVER (PARTITION BY hostname, scale, duration, clients, partitions, max_parallel_workers_per_gather ORDER BY parallel_leader_participation DESC)
+        FROM pgss_results) ORDER BY hostname,
+    scale,
+    clients,
+    partitions,
+    max_parallel_workers_per_gather::int,
+    parallel_leader_participation DESC) x
+GROUP BY
+    ROLLUP (scale, partitions)
+ORDER BY
+    scale,
+    partitions;
 
--- summary
-select *, (avg(no_participation_speedup_pct) over())::numeric(7,1) as avg_no_participation_speedup_pct from (
-select
-  scale,
-  partitions,
-  case when random_page_cost::float < 2 then 'fast' else 'slow' end as storage_latency, -- random_page_cost set by pg-spot-operator automatically according to disk type
-  avg(mean_exec_time / 1000.0)::int mean_exec_time_ms, avg(pct)::numeric(7,1) as no_participation_speedup_pct
-from (
-select
-  *,
-  lag as mean_exec_time_lag,
-  (100.0 * ((mean_exec_time - lag) / lag))::numeric(7,1) pct
-from (
-select
-  *,
-  lag(mean_exec_time) over(partition by hostname, scale, duration, random_page_cost, clients, partitions, max_parallel_workers_per_gather order by parallel_leader_participation desc)
-from
-  pgss_results
--- where scale = 5000
--- and partitions > 0
-)
-order by
-  hostname, scale, clients, partitions, max_parallel_workers_per_gather::int, parallel_leader_participation desc
-) x
-group by (scale, partitions, random_page_cost)
-order by scale, partitions, random_page_cost
-) y
-;
-/*
-scale|partitions|storage_latency|mean_exec_time_ms|no_participation_speedup_pct|avg_no_participation_speedup_pct|
------+----------+---------------+-----------------+----------------------------+--------------------------------+
- 5000|         0|fast           |               60|                        -0.3|                            -2.7|
- 5000|         0|slow           |              394|                        -1.1|                            -2.7|
- 5000|         8|fast           |               55|                        -2.6|                            -2.7|
- 5000|         8|slow           |              336|                        -6.9|                            -2.7|
-*/
+
+-- a more detailed summary
+SELECT
+    *
+FROM (
+    SELECT
+        scale,
+        partitions,
+        CASE WHEN random_page_cost::float < 2 THEN
+            'fast'
+        ELSE
+            'slow'
+        END AS storage_latency, -- random_page_cost set by pg-spot-operator automatically according to disk type
+        avg(mean_exec_time / 1000.0)::int mean_exec_time_ms,
+        avg(pct)::numeric(7, 1) AS no_participation_speedup_pct
+    FROM (
+        SELECT
+            *,
+            lag AS mean_exec_time_lag,
+            (100.0 * ((mean_exec_time - lag) / lag))::numeric(7, 1) pct
+        FROM (
+            SELECT
+                *,
+                lag(mean_exec_time) OVER (PARTITION BY hostname, scale, duration, random_page_cost, clients, partitions, max_parallel_workers_per_gather ORDER BY parallel_leader_participation DESC)
+            FROM pgss_results
+            -- where scale = 5000
+            -- and partitions > 0
+) ORDER BY hostname,
+        scale,
+        clients,
+        partitions,
+        max_parallel_workers_per_gather::int,
+        parallel_leader_participation DESC) x
+GROUP BY ROLLUP (scale, partitions, random_page_cost) ORDER BY scale, partitions, random_page_cost) y;
 
 
 -- target use case: partitions + more data
@@ -55,4 +78,43 @@ with q as (
 )
 select 100.0 * ((q.plp_off - q.plp_on) / q.plp_on) as diff from q;
 
--- -7.214798181364803285000
+
+-- max win conditions
+SELECT
+    *
+FROM (
+    SELECT
+        hostname,
+        scale,
+        partitions,
+        CASE WHEN random_page_cost::float < 2 THEN
+            'fast'
+        ELSE
+            'slow'
+        END AS storage_latency, -- random_page_cost set by pg-spot-operator automatically according to disk type
+        avg(mean_exec_time / 1000.0)::int mean_exec_time_ms,
+        avg(pct)::numeric(7, 1) AS no_leader_participation_speedup_pct
+    FROM (
+        SELECT
+            *,
+            lag AS mean_exec_time_lag,
+            (100.0 * ((mean_exec_time - lag) / lag))::numeric(7, 1) pct
+        FROM (
+            SELECT
+                *,
+                lag(mean_exec_time) OVER (PARTITION BY hostname, scale, duration, random_page_cost, clients, partitions, max_parallel_workers_per_gather ORDER BY parallel_leader_participation DESC)
+            FROM pgss_results) x ORDER BY hostname,
+        scale,
+        clients,
+        partitions,
+        max_parallel_workers_per_gather::int,
+        parallel_leader_participation DESC) y
+GROUP BY 1,
+2,
+3,
+4) z
+WHERE
+    no_leader_participation_speedup_pct < 0
+ORDER BY
+    no_leader_participation_speedup_pct nulls LAST
+LIMIT 5;
