82ef3817ebb60219570405b080eb7b92409befa4
Kaarel Moppel <kaarel.moppel@gmail.com>
2025-01-22 00:33:08 +0200

    mean_exec_time_ms > mean_exec_time_s in analyze_results.sql

--- analyze_results.sql
+++ analyze_results.sql
@@ -11,7 +11,7 @@ select 100.0 * ((q.plp_off - q.plp_on) / q.plp_on) as diff from q;
 SELECT
     scale,
     partitions,
-    avg(mean_exec_time / 1000.0)::int mean_exec_time_ms,
+    avg(mean_exec_time / 1000.0)::int mean_exec_time_s,
     avg(pct)::numeric(7, 1) AS no_leader_participation_speedup_pct
 FROM (
     SELECT
@@ -47,7 +47,7 @@ FROM (
         ELSE
             'slow'
         END AS storage_latency, -- random_page_cost set by pg-spot-operator automatically according to disk type
-        avg(mean_exec_time / 1000.0)::int mean_exec_time_ms,
+        avg(mean_exec_time / 1000.0)::int mean_exec_time_s,
         avg(pct)::numeric(7, 1) AS no_participation_speedup_pct
     FROM (
         SELECT
@@ -92,7 +92,7 @@ FROM (
         ELSE
             'slow'
         END AS storage_latency, -- random_page_cost set by pg-spot-operator automatically according to disk type
-        avg(mean_exec_time / 1000.0)::int mean_exec_time_ms,
+        avg(mean_exec_time / 1000.0)::int mean_exec_time_s,
         avg(pct)::numeric(7, 1) AS no_leader_participation_speedup_pct
     FROM (
         SELECT
